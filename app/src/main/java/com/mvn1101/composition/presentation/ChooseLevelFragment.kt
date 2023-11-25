8d2bba9d82f709d3fd1f43326f977a086ae14960
Viktor Mikhaylov <mikhaylovvn1101@gmail.com>
2023-11-25 18:12:37 +0500

    add view binding

--- app/src/main/java/com/mvn1101/composition/presentation/ChooseLevelFragment.kt
+++ app/src/main/java/com/mvn1101/composition/presentation/ChooseLevelFragment.kt
@@ -6,14 +6,30 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.mvn1101.composition.R
+import com.mvn1101.composition.databinding.FragmentChooseLevelBinding
+import com.mvn1101.composition.databinding.FragmentGameBinding
 
 class ChooseLevelFragment: Fragment() {
+
+    private var _binding: FragmentChooseLevelBinding? = null
+    private val binding: FragmentChooseLevelBinding
+        get() = _binding ?: throw RuntimeException("FragmentChooseLevelBinding == null")
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_choose_level, container, false)
+    ): View {
+        _binding = FragmentChooseLevelBinding.inflate(inflater, container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }

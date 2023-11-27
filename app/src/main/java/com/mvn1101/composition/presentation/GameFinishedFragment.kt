4cb498f4adfe029ae9bc1786b23bee48938d1b55
Viktor Mikhaylov <mikhaylovvn1101@gmail.com>
2023-11-27 22:16:50 +0500

    add Parcelable interface

--- app/src/main/java/com/mvn1101/composition/presentation/GameFinishedFragment.kt
+++ app/src/main/java/com/mvn1101/composition/presentation/GameFinishedFragment.kt
@@ -39,8 +39,11 @@ class GameFinishedFragment : Fragment() {
                 retryGame()
             }
         }
-        requireActivity().onBackPressedDispatcher.addCallback(
-            viewLifecycleOwner, callback)
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, callback)
+
+        binding.buttonRetry.setOnClickListener {
+            retryGame()
+        }
     }
 
     override fun onDestroyView() {
@@ -49,8 +52,9 @@ class GameFinishedFragment : Fragment() {
     }
 
     private fun parseArgs() {
-        gameResult =
-            requireArguments().getSerializable(GameFinishedFragment.KEY_GAME_RESULT) as GameResult
+        requireArguments().getParcelable<GameResult>(GameFinishedFragment.KEY_GAME_RESULT)?.let {
+            gameResult = it
+        }
     }
 
     private fun retryGame() {
@@ -67,7 +71,7 @@ class GameFinishedFragment : Fragment() {
         fun newInstance(gameResult: GameResult): GameFinishedFragment {
             return GameFinishedFragment().apply {
                 arguments = Bundle().apply {
-                    putSerializable(KEY_GAME_RESULT, gameResult)
+                    putParcelable(KEY_GAME_RESULT, gameResult)
                 }
             }
         }

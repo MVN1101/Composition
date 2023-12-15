8a5c0421fd3de15f653fa864f58c97ada1722002
Viktor Mikhaylov <mikhaylovvn1101@gmail.com>
2023-12-15 21:04:25 +0500

    add BindingAdapter to GameFragment

--- app/src/main/java/com/mvn1101/composition/presentation/BindingAdapters.kt
+++ app/src/main/java/com/mvn1101/composition/presentation/BindingAdapters.kt
@@ -1,10 +1,21 @@
 package com.mvn1101.composition.presentation
 
+import android.content.Context
+import android.content.DialogInterface.OnClickListener
+import android.content.res.ColorStateList
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
+import androidx.lifecycle.ViewModel
 import com.mvn1101.composition.R
 import com.mvn1101.composition.domain.entity.GameResult
+
+interface OnOptionClickListener {
+    fun onOptionClick(option: Int)
+}
 
 @BindingAdapter("requiredAnswers")
 fun bindRequiredAnswers(textView: TextView, count: Int) {
@@ -47,10 +58,44 @@ fun bindScorePercentage(textView: TextView, gameResult: GameResult) {
 fun bindResultEmoji(imageView: ImageView, winner: Boolean) {
     imageView.setImageResource(getSmileId(winner))
 }
+
 private fun getSmileId(winner: Boolean): Int {
     return if (winner) {
         R.drawable.ic_smile
     } else {
         R.drawable.ic_sad
+    }
+}
+
+@BindingAdapter("enoughCount")
+fun bindEnoughCount(textView: TextView, enough: Boolean) {
+    textView.setTextColor(getColorByState(textView.context, enough))
+}
+
+@BindingAdapter("enoughPercent")
+fun bindEnoughPercent(progressBar: ProgressBar, enough: Boolean) {
+    val color = getColorByState(progressBar.context, enough)
+    progressBar.progressTintList = ColorStateList.valueOf(color)
+
+}
+
+private fun getColorByState(context: Context, goodState: Boolean): Int {
+    val colorResId = if (goodState) {
+        android.R.color.holo_green_light
+    } else {
+        android.R.color.holo_red_light
+    }
+    return ContextCompat.getColor(context, colorResId)
+}
+
+@BindingAdapter("numberAsText")
+fun bindNumberAsText(textView: TextView, number: Int) {
+    textView.text = number.toString()
+}
+
+@BindingAdapter("onOptionClickListener")
+fun bindOnOptionClickListener(textView: TextView, clickListener: OnOptionClickListener) {
+    textView.setOnClickListener{
+        clickListener.onOptionClick(textView.text.toString().toInt())
     }
 }
